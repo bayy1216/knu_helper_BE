1998e38b495cdb65ef20eb297228953e17552aaa
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-30 02:15:58 +0900

    [Feat]: BaseTimeEntity 추가

---
+++ src/main/kotlin/com/reditus/knumate/domain/common/BaseTimeEntity.kt
@@ -0,0 +1,18 @@
+package com.reditus.knumate.domain.common
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseTimeEntity(
+    @CreatedDate
+    var createdDate: LocalDateTime? = null,
+    @LastModifiedDate
+    var lastModifiedDate: LocalDateTime? = null,
+) {
+}
