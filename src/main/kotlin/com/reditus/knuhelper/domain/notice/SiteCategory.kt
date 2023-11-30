4d38ece8aabdd38da325f0b9d7b5108394cc996f
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-30 10:01:28 +0900

    [Feat]: 공지 사이트 enum 추가

---
+++ src/main/kotlin/com/reditus/knuhelper/domain/notice/SiteCategory.kt
@@ -0,0 +1,26 @@
+package com.reditus.knuhelper.domain.notice
+
+enum class SiteCategory(val koreaName: String) {
+    HUMANITIES("인문대학"),
+    SOCIAL("사회과학대학"),
+    NATURE("자연과학대학"),
+    GYEONGSANG("경상대학"),
+    ENGINEER("공과대학"),
+    IT("IT대학"),
+    AGRICULTURE("농업생명과학대학"),
+    ART("예술대학"),
+    EDUCATION("사범대학"),
+    MEDICAL("의과대학"),
+    DENTAL("치과대학"),
+    VETERINARY("수의과대학"),
+    LIFESCIENCE("생활과학대학"),
+    NURSE("간호대학"),
+    PHARMACY("약학대학"),
+    ECOLOGY("생태환경대학"),
+    SCIENCETECH("과학기술대학"),
+    PUBLICADMIN("행정학부"),
+    AUTONOMOUS("자율전공"),
+    FAULTY("융합학부"),
+    KNU("경북대"),
+    SW("소프트웨어교육원")
+}
