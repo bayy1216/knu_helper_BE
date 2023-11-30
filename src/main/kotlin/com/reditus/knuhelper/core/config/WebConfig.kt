0c01503b6e38781c9a683d2f629eb52ea6a1e10e
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-30 12:07:19 +0900

    [Feat]: config 어노테이션 추가

--- src/main/kotlin/com/reditus/knuhelper/core/config/WebConfig.kt
+++ src/main/kotlin/com/reditus/knuhelper/core/config/WebConfig.kt
@@ -2,9 +2,11 @@ package com.reditus.knuhelper.core.config
 
 import com.reditus.knuhelper.core.resolver.TokenUserIdResolver
 import com.reditus.knuhelper.core.resolver.TokenUserRoleResolver
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(TokenUserIdResolver())
