75e89c196cd90f65d87eb6f5071cfbaee48922c3
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-01 00:23:35 +0900

    [Feat]: fcm 메시지 전송 함수 추가

--- src/main/kotlin/com/reditus/knuhelper/core/config/FirebaseConfig.kt
+++ src/main/kotlin/com/reditus/knuhelper/core/config/FirebaseConfig.kt
@@ -3,8 +3,10 @@ package com.reditus.knuhelper.core.config
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.FileInputStream
 
@@ -23,4 +25,9 @@ class FirebaseConfig(
 
         FirebaseApp.initializeApp(options)
     }
+
+    @Bean
+    fun firebaseMessaging(): FirebaseMessaging =
+        FirebaseMessaging.getInstance()
+
 }
---
+++ src/main/kotlin/com/reditus/knuhelper/service/FirebaseService.kt
@@ -0,0 +1,24 @@
+package com.reditus.knuhelper.service
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.Notification
+import org.springframework.stereotype.Service
+
+@Service
+class FirebaseService(
+    private val firebaseMessaging: FirebaseMessaging,
+) {
+
+    fun sendMessage(title: String, body: String, token: String){
+        val message = Message.builder()
+            .setNotification(Notification.builder().setTitle(title).setBody(body).build())
+            .setToken(token)
+            .build()
+        try{
+            val response = firebaseMessaging.send(message)
+        }catch (e: Exception){
+            println(e.message)
+        }
+    }
+}
