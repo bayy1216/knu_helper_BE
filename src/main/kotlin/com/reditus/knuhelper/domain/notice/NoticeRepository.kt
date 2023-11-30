607d8f1c1e7902fe09cf951868ec95de792ffe2d
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-30 11:33:32 +0900

    [Feat]: 공지조회 repository 기능 추가

--- src/main/kotlin/com/reditus/knuhelper/domain/notice/NoticeRepository.kt
+++ src/main/kotlin/com/reditus/knuhelper/domain/notice/NoticeRepository.kt
@@ -1,6 +1,11 @@
 package com.reditus.knuhelper.domain.notice
 
+import com.reditus.knuhelper.domain.user.UserSubscribedSite
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.jpa.repository.JpaRepository
+import java.awt.print.Pageable
 
 interface NoticeRepository : JpaRepository<Notice, Long> {
+    fun findAllBySitesInOrderByCreatedAtDesc(sites: List<Site>, pageRequest: PageRequest): Page<Notice>
 }
--- src/main/kotlin/com/reditus/knuhelper/domain/user/UserRepository.kt
+++ src/main/kotlin/com/reditus/knuhelper/domain/user/UserRepository.kt
@@ -1,6 +1,10 @@
 package com.reditus.knuhelper.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface UserRepository : JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u JOIN FETCH u.subscribedSite WHERE u.id = :userId")
+    fun findByIdWithSubscribedSites(@Param("userId") userId: Long): User?
 }
