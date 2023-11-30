ed3a58232995b82716f83813852a82d6560a8c6f
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-30 11:42:12 +0900

    [Feat]: 공지 추가 controller

--- src/main/kotlin/com/reditus/knuhelper/controller/NoticeController.kt
+++ src/main/kotlin/com/reditus/knuhelper/controller/NoticeController.kt
@@ -1,13 +1,20 @@
 package com.reditus.knuhelper.controller
 
 import com.reditus.knuhelper.core.annotation.TokenUserId
+import com.reditus.knuhelper.core.annotation.TokenUserRole
+import com.reditus.knuhelper.domain.user.UserRole
 import com.reditus.knuhelper.dto.common.PagingResponse
+import com.reditus.knuhelper.dto.notice.request.CreateNoticeRequest
 import com.reditus.knuhelper.dto.notice.response.NoticeDto
 import com.reditus.knuhelper.service.NoticeService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.lang.IllegalArgumentException
 
 @RestController
 @RequestMapping("/notice")
@@ -18,4 +25,11 @@ class NoticeController(
     fun getNotice(@TokenUserId userId: Long, @RequestParam("page") page:Int, @RequestParam("size", required = false) size :Int = 20) : PagingResponse<NoticeDto> =
         noticeService.getNotice(userId, page, size)
 
+    @PostMapping
+    fun createNotice(@TokenUserRole role: UserRole, @RequestBody request: CreateNoticeRequest) : Long{
+        if (role != UserRole.ROLE_ADMIN) {
+            throw IllegalArgumentException("권한이 없습니다.")
+        }
+        return noticeService.createNotice(request)
+    }
 }
