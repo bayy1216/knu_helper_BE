9f09b370dd49e3cdf490a845076e3d6a8277a451
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-01 22:21:24 +0900

    [Refactor]: fcmService 리팩토링
    - 작동하지 않던 로직 수정
    - fcmUtils 주입하여 클래스 책임 분리

--- src/main/kotlin/com/reditus/knuhelper/service/FcmService.kt
+++ src/main/kotlin/com/reditus/knuhelper/service/FcmService.kt
@@ -1,52 +1,36 @@
 package com.reditus.knuhelper.service
 
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.Message
-import com.google.firebase.messaging.Notification
 import com.reditus.knuhelper.domain.notice.NoticeRepository
 import com.reditus.knuhelper.domain.user.UserRepository
+import com.reditus.knuhelper.utils.FcmUtils
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
 class FcmService(
-    private val firebaseMessaging: FirebaseMessaging,
     private val userRepository: UserRepository,
     private val noticeRepository: NoticeRepository,
+    private val fcmUtils: FcmUtils,
 ) {
 
-    fun sendNoticeMessage(startTime: LocalDateTime, endTime: LocalDateTime){
+    fun sendNoticeMessage(startTime: LocalDateTime, endTime: LocalDateTime): Int{
         val users = userRepository.findAllWithSubscribedSites()
         val notices = noticeRepository.findAllByModifiedAtAfter(startTime)
-
+        var count = 0
         users.forEach{ user ->
-            val subscribedSites = user.subscribedSite
-
+            val subscribedSites = user.subscribedSite.filter { site -> site.isAlarm }.map { site -> site.site }
             //구독중인 사이트의 공지사항만 필터링
             val filteredNotice = notices.filter { notice ->
-                subscribedSites.any {
-                    subscribedSite -> subscribedSite.site == notice.site
-                }
+                notice.site in subscribedSites
             }
+            if(filteredNotice.isEmpty()) return@forEach
 
             val noticeTitles = filteredNotice.map { n -> n.title }
             val title = "${noticeTitles.size}개의 새로운 공지사항이 있습니다."
             val body = noticeTitles.reduce { acc, s -> "$acc\n$s" }
-            sendMessage(title, body, user.fcmToken)
+            fcmUtils.sendSingleMessage(title, body, user.fcmToken)
+            count++
         }
-    }
-
-
-
-    private fun sendMessage(title: String, body: String, token: String){
-        val message = Message.builder()
-            .setNotification(Notification.builder().setTitle(title).setBody(body).build())
-            .setToken(token)
-            .build()
-        try{
-            val response = firebaseMessaging.send(message)
-        }catch (e: Exception){
-            println(e.message)
-        }
+        return count
     }
 }
