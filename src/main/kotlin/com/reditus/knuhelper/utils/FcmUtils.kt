6b3e0af3c0b916acf472c63f4f2f65a406dc3e4e
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-01 22:16:28 +0900

    [Feat]: fcmUtils로 클래스 분리

---
+++ src/main/kotlin/com/reditus/knuhelper/utils/FcmUtils.kt
@@ -0,0 +1,26 @@
+package com.reditus.knuhelper.utils
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.MulticastMessage
+import com.google.firebase.messaging.Notification
+import org.springframework.stereotype.Component
+
+@Component
+class FcmUtils {
+    fun sendSingleMessage(title: String, body: String, token: String):String{
+        val message = Message.builder()
+            .setNotification(Notification.builder().setTitle(title).setBody(body).build())
+            .setToken(token)
+            .build()
+        return FirebaseMessaging.getInstance().send(message)
+    }
+    fun sendManyMessage(title: String, body: String, tokens: List<String>):Int{
+        val message = MulticastMessage.builder()
+            .setNotification(Notification.builder().setTitle(title).setBody(body).build())
+            .addAllTokens(tokens)
+            .build()
+        val response = FirebaseMessaging.getInstance().sendMulticast(message)
+        return response.successCount
+    }
+}
